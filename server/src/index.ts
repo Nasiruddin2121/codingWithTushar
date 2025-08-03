d242c3a8777f95f3f8b186a7bca8ec144cb4eaad
Md Nasir Uddin Swapan <nasiruddin21211@gmail.com>
2025-08-03 16:33:15 +0600

    updated

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express,{Application, Request, Response} from "express";
+import "dotenv/config";
+const app:Application = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send("Hello World");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
